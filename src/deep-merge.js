691a3a67cc108d40298ad700df62aa92df161d04
Pieter Wigboldus <w3n@w3news.org>
2023-11-24 15:50:46 +0100

    Add all files

---
+++ src/deep-merge.js
@@ -0,0 +1,24 @@
+/**
+ * @param {...object} objects - Objects to merge
+ * @returns {object} New object with merged key/values
+ */
+export const deepMerge = (...objects) => {
+  const isObject = obj => obj?.constructor === Object
+
+  return objects.reduce((accumulator, currentValue) => {
+    Object.keys(currentValue).forEach(key => {
+      const pVal = accumulator[key]
+      const oVal = currentValue[key]
+
+      if (Array.isArray(pVal) && Array.isArray(oVal)) {
+        accumulator[key] = pVal.concat(...oVal)
+      } else if (isObject(pVal) && isObject(oVal)) {
+        accumulator[key] = deepMerge(pVal, oVal)
+      } else {
+        accumulator[key] = oVal
+      }
+    })
+
+    return accumulator
+  }, {})
+}
