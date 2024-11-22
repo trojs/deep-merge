bc0dfabeb65f86920bdbf6052e0d354dbe79e862
Pieter Wigboldus <pieter.wigboldus@zilliz.nl>
2024-11-22 16:12:29 +0100

    Upgrade Eslint to version 9

--- src/deep-merge.test.js
+++ src/deep-merge.test.js
@@ -1,6 +1,6 @@
-import test from 'node:test';
-import assert from 'node:assert';
-import { deepMerge } from './index.js';
+import test from 'node:test'
+import assert from 'node:assert'
+import { deepMerge } from './index.js'
 
 test('deepMerge', async (t) => {
     await t.test('It should merge 2 objects', async () => {
@@ -8,47 +8,47 @@ test('deepMerge', async (t) => {
             a: 1,
             b: 1,
             c: { x: 1, y: 1 },
-            d: [1, 1],
-        };
+            d: [1, 1]
+        }
         const obj2 = {
             b: 2,
             c: { y: 2, z: 2 },
             d: [2, 2],
-            e: 2,
-        };
-        const result = deepMerge(obj1, obj2);
+            e: 2
+        }
+        const result = deepMerge(obj1, obj2)
 
         const expectedResult = {
             a: 1,
             b: 2,
             c: { x: 1, y: 2, z: 2 },
             d: [1, 1, 2, 2],
-            e: 2,
-        };
-        assert.deepEqual(result, expectedResult);
-    });
+            e: 2
+        }
+        assert.deepEqual(result, expectedResult)
+    })
 
     await t.test('It should merge 3 objects', async () => {
         const obj1 = {
             a: 1,
-            b: 1,
-        };
+            b: 1
+        }
         const obj2 = {
             b: 2,
-            c: 2,
-        };
+            c: 2
+        }
         const obj3 = {
             c: 3,
-            d: 3,
-        };
-        const result = deepMerge(obj1, obj2, obj3);
+            d: 3
+        }
+        const result = deepMerge(obj1, obj2, obj3)
 
         const expectedResult = {
             a: 1,
             b: 2,
             c: 3,
-            d: 3,
-        };
-        assert.deepEqual(result, expectedResult);
-    });
-});
+            d: 3
+        }
+        assert.deepEqual(result, expectedResult)
+    })
+})
