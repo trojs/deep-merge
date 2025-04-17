0ea640131b81b00e26db1b9e50ee63ee27f0d7ca
Pieter Wigboldus <w3n@w3news.org>
2025-04-17 09:02:26 +0200

    Use the TroJS lint rules

--- src/deep-merge.js
+++ src/deep-merge.js
@@ -5,22 +5,22 @@
  * @returns {T} New object with merged key/values
  */
 export const deepMerge = (...objects) => {
-    const isObject = (obj) => obj?.constructor === Object
+  const isObject = (obj) => obj?.constructor === Object
 
-    return objects.reduce((accumulator, currentValue) => {
-        Object.keys(currentValue).forEach((key) => {
-            const pVal = accumulator[key]
-            const oVal = currentValue[key]
+  return objects.reduce((accumulator, currentValue) => {
+    Object.keys(currentValue).forEach((key) => {
+      const pVal = accumulator[key]
+      const oVal = currentValue[key]
 
-            if (Array.isArray(pVal) && Array.isArray(oVal)) {
-                accumulator[key] = pVal.concat(...oVal)
-            } else if (isObject(pVal) && isObject(oVal)) {
-                accumulator[key] = deepMerge(pVal, oVal)
-            } else {
-                accumulator[key] = oVal
-            }
-        })
+      if (Array.isArray(pVal) && Array.isArray(oVal)) {
+        accumulator[key] = pVal.concat(...oVal)
+      } else if (isObject(pVal) && isObject(oVal)) {
+        accumulator[key] = deepMerge(pVal, oVal)
+      } else {
+        accumulator[key] = oVal
+      }
+    })
 
-        return accumulator
-    }, {})
+    return accumulator
+  }, {})
 }
